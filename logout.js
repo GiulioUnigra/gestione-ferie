4e3775b5e298593148497addbd4774872978087e
GiulioUnigra <giulio.montanari@unigra.it>
2025-07-17 17:08:56 +0200

    Update logout.js

--- logout.js
+++ logout.js
@@ -1,8 +1,9 @@
 import { createClient } from 'https://esm.sh/@supabase/supabase-js@2';
 
 const supabase = createClient(
-  'https://lhwdoxwqhzskksjcuirj.supabase.co',
-  'YOUR_PUBLIC_ANON_KEY'
+  'https://sauahmyekejigpbjrdug.supabase.co',
+    'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InNhdWFobXlla2VqaWdwYmpyZHVnIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTE0Njc2OTcsImV4cCI6MjA2NzA0MzY5N30.JetFvFBwg1dy9AqyeSnTYalAuuIrPAyaFWzc8SRaxYo'           // <- 🔁 aggiorna
+  );
 );
 
 (async () => {
